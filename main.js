c830e5283d5c600cda90f02e24ca365d7b43c8c6
Daphne Bohm <daphne_bohm@hotmail.com>
2021-01-04 15:22:29 +0100

    removed classes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const menu = document.querySelector("[rel='menu']");
 const nav = document.querySelector('.nav__list');
 
-console.log(nav);
-
 menu.addEventListener('click', () =>{
     nav.classList.toggle('visible-js-toggle');
 })
